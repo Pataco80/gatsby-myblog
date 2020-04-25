b3af80dc9206f1c7be8bec7996fdc8b05841873f
Ricardo Do Vale <pataco80@gmail.com>
2020-04-25 03:11:51 +0200

    test de la page node

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,29 +1,48 @@
-const path = require("path")
+/**
+ * Implement Gatsby's Node APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/node-apis/
+ */
+// path est requis pour créer des nouvelles pages avec la méthode ".createPages()"
+const path = require('path')
+
+// You can delete this file if you're not using it
+// CreateFilePath sert à créer des urls des pages en se référent au systeme de fichiers pour aller chercher le dossier 'posts'
 const { createFilePath } = require(`gatsby-source-filesystem`)
+
+// Variables de chemin pour les templates
+const BlogPostTemplate = path.resolve(`./src/templates/blog-post.js`)
 
 // Création du champs slug pour chaques posts
 exports.onCreateNode = ({ node, getNode, actions }) => {
+
   const { createNodeField } = actions
-  // Ensures we are processing only markdown files
-  if (node.internal.type === "MarkdownRemark") {
-    // Use `createFilePath` to turn markdown files in our `data/faqs` directory into `/faqs/slug`
+
+  // Vérifie si le type de fichier et Markdown,... si oui, il crée  le chemin pour le slug en se basant sur la création de 'pages'
+  if (node.internal.type === 'MarkdownRemark') {
+
+    // si oui, il crée  le chemin pour le slug en se basant sur la création de 'pages'
     const slug = createFilePath({
       node, 
       getNode, 
-      basePath: "pages",
+      basePath: 'pages' 
     })
-
-    // Creates new query'able field with name of 'slug'
+    
+    // Cràatopm du champ "slug" pour la requête GraphQl à chaque post, considéré comme un "item", appelé "node" ou noeud.
     createNodeField({
-      node,
-      name: "slug",
-      value: `/${slug.slice(12)}`,
+      node, // chaque post est un noeud
+      name: 'slug',
+      value: `/${slug.slice(12)}`, // affichage de l'url ex: "http://www.monsite.com`/nom-du-fichier`" stocké dans la variable "${slug}".
+      // Utilisation de la méthode ".slice(12)" pour sauter les 12 premiers caractères du nom du fichier afin d'éviter la suite de chiffres de la date. Seul le texte titre du post s'affichera.
     })
   }
 }
 
+// Création des pages de site. Méthode ".createPage" va se servir des données graphQl pour retourner une page spécifique.
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
+
+  // Requête graphQl pour obtenir le slug du post, comme le ferait un identifiant unique et créer ainsi la page à partir de la donnée slug reçue.
   return graphql(`
     {
       allMarkdownRemark(sort: {fields: frontmatter___date, order: DESC}) {
@@ -44,30 +63,35 @@ exports.createPages = ({ graphql, actions }) => {
         }
       }
     }
-  `).then(result => {
-    const posts = result.data.allMarkdownRemark.edges
+  `).then(postList => {
+ // La requête étant asynchrone, la methode ".then()" s'applique. Nous nomons "postList" le résultat de la requête et la passons en paramêtre de la méthode et exécutons une fonction.
 
-    posts.forEach(({ node }) => {
+
+    // On récupère la liste et faisons une boucle du "eges", un tableau contenant tous les posts. Chaque post considéré comme un "item", appelé "node" ou noeud est passé en argument comme un objet
+    postList.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      // A la création de la page, il y a 3 paramêtres,
+      // "path:" reçois la valeur de l'url, la valeur du champ "slug" en locurance.
+      // "component:" Il s'agit de la page qui servira de template
+      // "cpmtext:" est un objet dont on passe les variables pour la requête graphQl
       createPage({
         path: node.fields.slug,
-        component: path.resolve(`./src/templates/blog-post.js`),
+        component: BlogPostTemplate,
         context: {
-          // Data passed to context is available
-          // in page queries as GraphQL variables.
+          // Ci dessous nous passons le slug du post comme valeur de la requête graphQl. Il peut y avoir plusieurs variables dans le contexte, ici, seul le slug est nécéssaire pour créer la page.
           slug: node.fields.slug,
         },
       })
     })
 
     const PostsPerPage = 6
-    const numPages = Math.ceil(posts.length / PostsPerPage)
+    const numPages = Math.ceil(postList.length / PostsPerPage)
 
-    Array.from({lenght: numPages}).forEach((_,index) => {
+    Array.from({ length: numPages }).forEach((_, index) => {
       createPage({
         path: index === 0 ? `/` : `/page/${index + 1}`,
         component: path.resolve(`./src/templates/blog-list.js`),
         context: {
-          limit:PostsPerPage,
+          limit: PostsPerPage,
           skip: index * PostsPerPage,
           numPages,
           currentPage: index + 1,
